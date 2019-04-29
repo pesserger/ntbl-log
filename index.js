38f95b706939da382a6efb6d15f428517a9e167d
sunny <hsy.ntbl@gmail.com>
2019-04-29 18:18:58 +0800

    v1.0.0

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const cliSpinners = require('cli-spinners')
 const logUpdate = require('log-update')
 const chalk = require('chalk')
 
+module.exports = Log
+
+
 function Log(options = {}) {
   if (!(this instanceof Log)) return new Log(options)
 
@@ -40,15 +43,3 @@ Log.prototype = {
     logUpdate(text)
   }
 }
-
-
-const log = Log({
-  name: 'dots',
-  speed: 80,
-  color: 'green'
-})
-
-// 开始
-log.start(frame => `${frame} 下载中...`)
-// 两秒后，停止
-setTimeout(() => log.stop(true), 2000)
